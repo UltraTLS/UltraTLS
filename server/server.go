6f8df463de01937db4aa4881f330d367c934f5f4
NewTCP <newtcp1@hotmail.com>
2025-07-09 12:43:36 +0800

    Update server.go

--- server/server.go
+++ server/server.go
@@ -3,9 +3,9 @@ package server
 import (
 	"io"
 	"log"
-	"myproxy/api"
-	"myproxy/internal/netutil"
-	"myproxy/protocol"
+	"github.com/UltraTLS/UltraTLS/api"
+	"github.com/UltraTLS/UltraTLS/internal/netutil"
+	"github.com/UltraTLS/UltraTLS/protocol"
 	"net"
 )
 
